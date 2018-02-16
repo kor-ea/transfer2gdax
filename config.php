c19e0398891eeb82a6edfcc5fdd6b7dd95fa06e7
biTree <evgeny@kornyushin.com>
2018-02-16 23:01:33 +0300

    maxcap

--- config.php
+++ config.php
@@ -1,17 +1,14 @@
 <?php 
 //GDAX API keys
-define('APIKEY','6eaa42402c76166d2719b7eff2397b3f');
-define('APISECRET','JtURqJCN0FztSYwVu1eRRgCCtqqEPygr81+X1TmSLSTRxrjrocUgpyjXCzRhJUHHMODVe+wJo0rJxipFvnuLUQ==');
-define('APIPASSWORD' ,'7w29jk5o0z');
+define('APIKEY','');
+define('APISECRET','');
+define('APIPASSWORD' ,'');
 //Max cap for selling
 define('MAXCAP',250000);
 //Pair to trade
 define('PAIR','BTC/USD');
 //New Relic Insights API keys
-define('ACCOUNTID', '1879097');
-define('INSERTKEY', '3QC46cXMkeZg6aiYk-qsAof6070dor73');
-define('QUERYKEY', 'iSg_jr6D61zeBpWJUmV5c0rhM5a9wl0H');
-
-
-
+define('ACCOUNTID', '');
+define('INSERTKEY', '');
+define('QUERYKEY', '');
 ?>
